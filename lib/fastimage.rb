e9014e589810f1126718948b0b5229d59eb9dc4c
Benjamin Kristoffersen <benjamin@amasome.com>
2015-01-19 13:35:41 +0100

    Added a user-agent 2

--- lib/fastimage.rb
+++ lib/fastimage.rb
@@ -305,7 +305,7 @@ class FastImage
   end
 
   def fetch_using_open_uri
-    open(@uri, 'User-Agent' => 'opera') do |s|
+    open(URI.parse(URI.encode(@uri.strip)), 'User-Agent' => 'opera') do |s|
       fetch_using_read(s)
     end
   end
